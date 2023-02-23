8bf54191d345714e431e958a963147e89ede7f4b
wu_nan@u.nus.edu <wu_nan@u.nus.edu>
2023-02-24 01:05:56 +0800

    initial commit

---
+++ assign1_common_words/TopkCommonWords.java
@@ -0,0 +1,24 @@
+/*
+ENTER YOUR NAME HERE
+NAME: WU NAN
+MATRICULATION NUMBER: A0205048L
+*/
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class TopkCommonWords {
+    public static void main(String[] args){
+
+    } 
+}
